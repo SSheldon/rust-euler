aa2494faec7d603990c2b891293a88e9194b3d9d
Steven Sheldon <steven@sasheldon.com>
2014-04-28 09:20:14 -0700

    Improved problem 1 to use iterators.

--- p1.rs
+++ p1.rs
@@ -1,16 +1,6 @@
+use std::iter::AdditiveIterator;
+
 fn main() {
-	let mut sum = 0;
-	let mut i = 3;
-	while i < 1000 {
-		sum += i;
-		i += 3;
-	}
-	i = 5;
-	while i < 1000 {
-		if i % 3 != 0 {
-			sum += i;
-		}
-		i += 5;
-	}
+	let sum = range(0, 1000).filter(|&x| x % 3 == 0 || x % 5 == 0).sum();
 	println!("{}", sum);
 }
