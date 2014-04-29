4d45b12e1336e13376071b2cfe558cec28435ac3
Steven Sheldon <steven@sasheldon.com>
2014-04-29 01:54:05 -0700

    Improved solution for problem 3.
    Only iterate to square root and extracted prime_factorization function.

--- p3.rs
+++ p3.rs
@@ -1,21 +1,29 @@
 use std::iter::range_inclusive;
+use std::num::sqrt;
 
 fn least_divisor(n : uint) -> uint {
-	match range_inclusive(2, n / 2).find(|&x| n % x == 0) {
+	match range_inclusive(2, sqrt(n as f64) as uint).find(|&x| n % x == 0) {
 		Some(x) => x,
 		None => n,
 	}
 }
 
-fn main() {
-	let mut remainder = 600851475143;
+fn prime_factorization(n : uint) -> ~[uint] {
+	let mut remainder = n;
+	let mut factors = ~[];
 	loop {
 		let divisor = least_divisor(remainder);
 		if divisor == remainder {
-			println!("{}", remainder);
+			factors.push(remainder);
 			break;
 		} else {
 			remainder /= divisor;
+			factors.push(divisor);
 		}
 	}
+	factors
+}
+
+fn main() {
+	println!("{}", prime_factorization(600851475143).last().unwrap());
 }
