dffb295c8f3086788dc27f28128ccd056b5912eb
Steven Sheldon <steven@sasheldon.com>
2014-04-29 23:38:44 -0700

    Solved problem 4.

--- p4.rs
+++ p4.rs
@@ -1,3 +1,32 @@
+struct CombinationRange {
+	start: uint,
+	stop: uint,
+	first: uint,
+	second: uint,
+}
+
+impl Iterator<(uint, uint)> for CombinationRange {
+	fn next(&mut self) -> Option<(uint, uint)> {
+		let (first, second) = if self.second < self.stop {
+			(self.first, self.second)
+		} else {
+			(self.first + 1, self.start)
+		};
+
+		if first < self.stop && second < self.stop {
+			self.first = first;
+			self.second = second + 1;
+			Some((first, second))
+		} else {
+			None
+		}
+	}
+}
+
+fn combination_range(start: uint, stop: uint) -> CombinationRange {
+	CombinationRange { start: start, stop: stop, first: start, second: start}
+}
+
 struct Digits {
 	remainder: uint,
 }
@@ -21,4 +50,9 @@ fn is_palindrome(n: uint) -> bool {
 }
 
 fn main() {
+	let combs = combination_range(100, 1000);
+	let max_palindrome = combs.map(|(x, y)| x * y)
+	                          .filter(|&x| is_palindrome(x))
+	                          .max().unwrap();
+	println!("{}", max_palindrome);
 }
