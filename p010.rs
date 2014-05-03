2633c278e1f0a8ef034a8e4ed5a21b84811cf744
Steven Sheldon <steven@sasheldon.com>
2014-05-03 14:59:22 -0700

    Optimized problem 10 to use a Primes iterator.

--- p010.rs
+++ p010.rs
@@ -1,14 +1,43 @@
+extern crate collections;
+
 use std::iter::{
 	AdditiveIterator,
-	range_inclusive,
+	range_step,
 };
-use std::num::sqrt;
+use collections::bitv::Bitv;
 
-fn is_prime(n: uint) -> bool {
-	range_inclusive(2, sqrt(n as f64) as uint).all(|x| n % x != 0)
+struct Primes {
+	current: uint,
+	stop: uint,
+	primes: Bitv,
+}
+
+impl Primes {
+	fn new(stop: uint) -> Primes {
+		let mut primes = Bitv::new(stop, true);
+		primes.set(0, false);
+		primes.set(1, false);
+		Primes{current: 1, stop: stop, primes: primes}
+	}
+}
+
+impl Iterator<uint> for Primes {
+	fn next(&mut self) -> Option<uint> {
+		match range(self.current + 1, self.stop).find(|&x| self.primes.get(x)) {
+			None => None,
+			Some(x) => {
+				// Mark all multiples of x as not prime
+				for i in range_step(x + x, self.stop, x) {
+					self.primes.set(i, false);
+				}
+				self.current = x;
+				Some(x)
+			}
+		}
+	}
 }
 
 fn main() {
-	let sum = range(2u, 2000000u).filter(|&x| is_prime(x)).sum();
+	let sum = Primes::new(2000000).sum();
 	println!("{}", sum);
 }
