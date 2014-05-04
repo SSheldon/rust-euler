3495e51d4310c5fdc9996ce39b5d60e12c588362
Steven Sheldon <steven@sasheldon.com>
2014-05-03 20:34:50 -0700

    Added freq_count function for problem 5.

--- p005.rs
+++ p005.rs
@@ -1,4 +1,16 @@
+extern crate collections;
+
+use std::hash::Hash;
 use std::iter::count;
+use collections::hashmap::HashMap;
+
+fn freq_count<A: TotalEq + Hash, T: Iterator<A>>(mut itr: T) -> HashMap<A, uint> {
+	let mut map = HashMap::new();
+	for item in itr {
+		map.insert_or_update_with(item, 1, |_, count| *count += 1);
+	}
+	map
+}
 
 fn main() {
 	let min = count(1, 1).find(|&x| range(1, 21).all(|y| x % y == 0)).unwrap();
