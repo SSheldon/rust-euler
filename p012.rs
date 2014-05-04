dd9abf435f5a1fc664f0fc5aadb26522bc9e0d61
Steven Sheldon <steven@sasheldon.com>
2014-05-04 14:54:36 -0700

    Added is_triangular function for problem 12.

--- p012.rs
+++ p012.rs
@@ -1,4 +1,13 @@
 use std::iter::range_inclusive;
+use std::num::sqrt;
+
+fn is_triangular(n: uint) -> bool {
+	// If n is triangular, there must be some i such that i(i+1)/2=n
+	// So by the quadratic formula, (sqrt(1+8n)-1)/2 must be an integer
+	let d = 8 * n + 1;
+	let sqrt_d = sqrt(d as f64) as uint;
+	(sqrt_d * sqrt_d == d) && ((sqrt_d - 1) % 2 == 0)
+}
 
 fn num_factors(n: uint) -> uint {
 	// Only factor after n/2 is n, so just stop halfway and add 1
