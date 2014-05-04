c3bdf57b36c91984a6e8e64ecb7a39f51ed84642
Steven Sheldon <steven@sasheldon.com>
2014-05-03 21:36:21 -0700

    Added max_values function for problem 5.

--- p005.rs
+++ p005.rs
@@ -12,6 +12,23 @@ fn freq_count<A: TotalEq + Hash, T: Iterator<A>>(mut itr: T) -> HashMap<A, uint>
 	map
 }
 
+fn max_values<K: TotalEq + Hash + Clone, V: Ord + Clone, T: Iterator<HashMap<K, V>>>(mut itr: T) -> HashMap<K, V> {
+	let mut max: HashMap<K, V> = HashMap::new();
+	for map in itr {
+		for (key, value) in map.iter() {
+			if max.contains_key(key) {
+				let existing = max.get_mut(key);
+				if existing.lt(value) {
+					*existing = value.clone();
+				}
+			} else {
+				max.insert(key.clone(), value.clone());
+			}
+		}
+	}
+	max
+}
+
 fn main() {
 	let min = count(1, 1).find(|&x| range(1, 21).all(|y| x % y == 0)).unwrap();
 	println!("{}", min);
