c766fb1d365354e0f692f96277e730d8cb2fd12c
Steven Sheldon <steven@sasheldon.com>
2014-05-06 00:42:27 -0700

    Added Memoization struct for problem 15.

--- p015.rs
+++ p015.rs
@@ -1,28 +1,45 @@
+struct Memoization<'a, A, B> {
+	cache: Vec<Option<B>>,
+	index: 'a |&A| -> uint,
+	calc: 'a |A, |A| -> B| -> B,
+}
+
+impl<'a, A, B: Clone> Memoization<'a, A, B> {
+	fn get(&mut self, arg: A) -> B {
+		let index = (self.index)(&arg);
+		if index >= self.cache.len() || self.cache.get(index).is_none() {
+			let result = (self.calc)(arg, |x| self.get(x));
+			self.cache.grow_set(index, &None, Some(result));
+		}
+		let cached = self.cache.get(index);
+		cached.get_ref().clone()
+	}
+}
+
+fn memoize<'r, A, B>(index: 'r |&A| -> uint, calc: 'r |A, |A| -> B| -> B) -> Memoization<'r, A, B> {
+	Memoization{
+		cache: Vec::new(),
+		index: index,
+		calc: calc,
+	}
+}
+
 static SIZE: uint = 21;
 
-fn num_routes(width: uint, height: uint, cache: &mut Vec<uint>) -> uint {
-	let index = SIZE * height + width;
-	let cached = if index < cache.len() {
-		*cache.get(index)
+fn num_routes((w, h): (uint, uint), f: |(uint, uint)| -> uint) -> uint {
+	if w == 0 || h == 0 {
+		1
 	} else {
-		0
-	};
-	if cached > 0 {
-		cached
-	} else {
-		let result = if width == 0 || height == 0 {
-			1
-		} else {
-			let right = num_routes(width - 1, height, cache);
-			let down  = num_routes(width, height - 1, cache);
-			right + down
-		};
-		cache.grow_set(index, &0, result);
-		result
+		let right = f((w - 1, h));
+		let down  = f((w, h - 1));
+		right + down
 	}
 }
 
 fn main() {
-	let mut routes: Vec<uint> = Vec::new();
-	println!("{}", num_routes(20, 20, &mut routes));
+	let mut mem = memoize::<(uint, uint), uint>(
+		|&(w, h)| SIZE * h + w,
+		num_routes,
+	);
+	println!("{}", mem.get((20, 20)));
 }
