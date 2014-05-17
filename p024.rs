85cd23546f47e34f4b97e4b79e6503d10a80a3f3
Steven Sheldon <steven@sasheldon.com>
2014-05-17 00:15:55 -0500

    Improved lex_permutation function for problem 24.

--- p024.rs
+++ p024.rs
@@ -3,16 +3,18 @@ use std::iter::{
 	range_inclusive,
 };
 
-fn permutation(num_digits: uint, mut index: uint) {
+fn lex_permutation(num_digits: uint, mut index: uint) -> ~str {
 	let mut digits: Vec<uint> = range(0, num_digits).collect();
 	let mut d = range_inclusive(1, num_digits).product();
+	let mut result = ~"";
 	for i in range_inclusive(1, num_digits).rev() {
 		d /= i;
-		println!("{}", digits.remove(index / d).unwrap());
+		result.push_str(digits.remove(index / d).unwrap().to_str());
 		index %= d;
 	}
+	result
 }
 
 fn main() {
-	permutation(10, 1000000 - 1);
+	println!("{}", lex_permutation(10, 1000000 - 1));
 }
