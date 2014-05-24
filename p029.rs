4a6ca7307491423f307ea4bee958302e6148be00
Steven Sheldon <steven@sasheldon.com>
2014-05-24 03:23:43 -0700

    Optimized problem 29.

--- p029.rs
+++ p029.rs
@@ -1,16 +1,17 @@
 extern crate collections;
 extern crate num;
 
-use std::num::pow;
 use collections::hashmap::HashSet;
 use num::bigint::ToBigUint;
 
 fn main() {
 	let mut pows = HashSet::new();
 	for a in range(2u, 101u).map(|a| a.to_biguint().unwrap()) {
-		for b in range(2u, 101u) {
+		let mut pow = a.clone();
+		for _ in range(2u, 101u) {
+			pow = pow * a;
 			// BigUint isn't hashable, so use a string instead
-			pows.insert(pow(a.clone(), b).to_str());
+			pows.insert(pow.to_str());
 		}
 	}
 	println!("{}", pows.len());
