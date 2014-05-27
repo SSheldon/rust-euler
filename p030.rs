985786c4ed8820d3c98627ed256040460c52c10d
Steven Sheldon <steven@sasheldon.com>
2014-05-26 21:51:52 -0700

    Added digit_power_sum function for problem 30.

---
+++ p030.rs
@@ -0,0 +1,24 @@
+use std::iter::AdditiveIterator;
+use euler::Digits;
+
+mod euler;
+
+static DIGIT_POWS: [uint, ..10] = [
+	0,
+	1,
+	2*2*2*2*2,
+	3*3*3*3*3,
+	4*4*4*4*4,
+	5*5*5*5*5,
+	6*6*6*6*6,
+	7*7*7*7*7,
+	8*8*8*8*8,
+	9*9*9*9*9,
+];
+
+fn digit_power_sum(n: uint) -> uint {
+	Digits::new(n).map(|x| DIGIT_POWS[x]).sum()
+}
+
+fn main() {
+}
